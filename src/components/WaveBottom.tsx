5dfb34ba4451113a484e967bf9862a9af69ad957
Jari Peltola <jena9988@gmail.com>
2026-06-08 22:04:38 +0300

    Wave height

--- src/components/WaveBottom.tsx
+++ src/components/WaveBottom.tsx
@@ -5,7 +5,7 @@ export default function WaveBottom({ color = "#faf8f3" }: { color?: string }) {
         viewBox="0 0 1440 120"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        className="w-full"
+        className="w-full origin-bottom scale-y-90"
       >
         <path
           d="M0 120L60 110C120 100 240 80 360 70C480 60 600 60 720 65C840 70 960 80 1080 80C1200 80 1320 70 1380 65L1440 60V120H1380C1320 120 1200 120 1080 120C960 120 840 120 720 120C600 120 480 120 360 120C240 120 120 120 60 120H0Z"
